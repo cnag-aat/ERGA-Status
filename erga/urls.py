699a02891bdb2956c9f02faa9f988b7788d41c03
Tyler Alioto <tyler.alioto@gmail.com>
2022-07-06 16:09:58 +0200

    updated views

--- erga/urls.py
+++ erga/urls.py
@@ -28,7 +28,7 @@ router.register(r'species', api.TargetSpeciesViewSet)
 urlpatterns = [
     url(r'^api/', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('', include('status.urls')),
-    path('admin/', admin.site.urls),
+    path('erga-status/', include('status.urls')),
+    path('erga-status/admin/', admin.site.urls),
     url(r'^accounts/', include('allauth.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- status/views.py
+++ status/views.py
@@ -1,5 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.views.generic import ListView
+from status.models import *
+from django.views.generic import TemplateView
+from django_tables2 import SingleTableView
+from django_tables2 import RequestConfig
+
+from status.tables import TargetSpeciesTable
+from django_tables2.export.views import ExportMixin
+from django_filters.views import FilterView
+from django_tables2.views import SingleTableMixin
+#from status.filters import SpeciesFilter
+from django.utils.safestring import mark_safe
+from math import log
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.contrib.auth.decorators import permission_required
+from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.db.models import Count
+from django.db.models import Q
+from django.conf import settings
+from django.core.files.storage import FileSystemStorage
+import subprocess
+import json
+import os, time, sys
+import scipy.stats as stats
+import networkx as nx
+from timeit import default_timer as timer
+from datetime import timedelta
+import pickle
+import logging
+from django.contrib import messages
+# Get an instance of a logger
+logger = logging.getLogger(__name__)
+
 
 
 def index(request):
