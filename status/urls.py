886ba28ded7a402027938bf76e610d6a66b06662
Tyler Alioto <tyler.alioto@gmail.com>
2022-07-07 12:13:36 +0200

    fixed navbar and added home url

--- status/urls.py
+++ status/urls.py
@@ -5,5 +5,6 @@ from status.views import TargetSpeciesListView
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path("", HomeView.as_view(), name="home"),
     path("species/", TargetSpeciesListView.as_view(), name="species_list"),
 ]
