000b2b81ae2700f1c29093c1158e0a8e020d6f38
Tyler Alioto <tyler.alioto@gmail.com>
2022-07-07 15:54:25 +0200

    changed order of models

--- status/models.py
+++ status/models.py
@@ -4,6 +4,84 @@ from django.conf import settings
 
 
 # Create your models here.
+
+class TaxonKingdom(models.Model):
+    name = models.CharField(max_length=100)
+    class Meta:
+        verbose_name_plural = 'kingdoms'
+
+    def __str__(self):
+        return self.name
+
+class TaxonPhylum(models.Model):
+    taxon_kingdom = models.ForeignKey(TaxonKingdom, blank=True, null=True, on_delete=models.CASCADE, verbose_name="Kingdom")
+    name = models.CharField(max_length=100)
+    class Meta:
+        verbose_name_plural = 'phyla'
+
+    def __str__(self):
+        return self.name
+
+class TaxonClass(models.Model):
+    taxon_kingdom = models.ForeignKey(TaxonKingdom, blank=True, null=True, on_delete=models.CASCADE, verbose_name="Kingdom")
+    taxon_phylum = models.ForeignKey(TaxonPhylum, blank=True, null=True, on_delete=models.CASCADE, verbose_name="Kingdom")
+    name = models.CharField(max_length=100)
+    class Meta:
+        verbose_name_plural = 'classes'
+
+    def __str__(self):
+        return self.name
+
+class TaxonOrder(models.Model):
+    taxon_kingdom = models.ForeignKey(TaxonKingdom, blank=True, null=True, on_delete=models.CASCADE, verbose_name="Kingdom")
+    taxon_phylum = models.ForeignKey(TaxonPhylum, blank=True, null=True, on_delete=models.CASCADE, verbose_name="Kingdom")
+    taxon_class = models.ForeignKey(TaxonClass, blank=True, null=True, on_delete=models.CASCADE, verbose_name="Kingdom")
+    name = models.CharField(max_length=100)
+    class Meta:
+        verbose_name_plural = 'orders'
+
+    def __str__(self):
+        return self.name
+
+class TaxonFamily(models.Model):
+    taxon_kingdom = models.ForeignKey(TaxonKingdom, blank=True, null=True, on_delete=models.CASCADE, verbose_name="Kingdom")
+    taxon_phylum = models.ForeignKey(TaxonPhylum, blank=True, null=True, on_delete=models.CASCADE, verbose_name="Kingdom")
+    taxon_class = models.ForeignKey(TaxonClass, blank=True, null=True, on_delete=models.CASCADE, verbose_name="Kingdom")
+    taxon_order = models.ForeignKey(TaxonOrder, blank=True, null=True, on_delete=models.CASCADE, verbose_name="Kingdom")
+    name = models.CharField(max_length=100)
+    class Meta:
+        verbose_name_plural = 'families'
+
+    def __str__(self):
+        return self.name
+
+class TaxonGenus(models.Model):
+    taxon_kingdom = models.ForeignKey(TaxonKingdom, blank=True, null=True, on_delete=models.CASCADE, verbose_name="Kingdom")
+    taxon_phylum = models.ForeignKey(TaxonPhylum, blank=True, null=True, on_delete=models.CASCADE, verbose_name="Kingdom")
+    taxon_class = models.ForeignKey(TaxonClass, blank=True, null=True, on_delete=models.CASCADE, verbose_name="Kingdom")
+    taxon_order = models.ForeignKey(TaxonOrder, blank=True, null=True, on_delete=models.CASCADE, verbose_name="Kingdom")
+    taxon_family = models.ForeignKey(TaxonFamily, blank=True, null=True, on_delete=models.CASCADE, verbose_name="Kingdom")
+    name = models.CharField(max_length=100)
+    class Meta:
+        verbose_name_plural = 'genera'
+
+    def __str__(self):
+        return self.name
+
+class TaxonSpecies(models.Model):
+    taxon_kingdom = models.ForeignKey(TaxonKingdom, blank=True, null=True, on_delete=models.CASCADE, verbose_name="Kingdom")
+    taxon_phylum = models.ForeignKey(TaxonPhylum, blank=True, null=True, on_delete=models.CASCADE, verbose_name="Kingdom")
+    taxon_class = models.ForeignKey(TaxonClass, blank=True, null=True, on_delete=models.CASCADE, verbose_name="Kingdom")
+    taxon_order = models.ForeignKey(TaxonOrder, blank=True, null=True, on_delete=models.CASCADE, verbose_name="Kingdom")
+    taxon_family = models.ForeignKey(TaxonFamily, blank=True, null=True, on_delete=models.CASCADE, verbose_name="Kingdom")
+    taxon_genus = models.ForeignKey(TaxonGenus, blank=True, null=True, on_delete=models.CASCADE, verbose_name="Kingdom")
+    name = models.CharField(max_length=100)
+    class Meta:
+        verbose_name_plural = 'species'
+
+    def __str__(self):
+        return self.name
+
 class TargetSpecies(models.Model):
     scientific_name = models.CharField(max_length=201, blank=True, null=True)
     tolid_prefix = models.CharField(max_length=12, blank=True, null=True)
@@ -43,83 +121,6 @@ class Synonyms(models.Model):
 
     class Meta:
         verbose_name_plural = 'synonyms'
-
-    def __str__(self):
-        return self.name
-
-class TaxonSpecies(models.Model):
-    taxon_kingdom = models.ForeignKey(TaxonKingdom, blank=True, null=True, on_delete=models.CASCADE, verbose_name="Kingdom")
-    taxon_phylum = models.ForeignKey(TaxonPhylum, blank=True, null=True, on_delete=models.CASCADE, verbose_name="Kingdom")
-    taxon_class = models.ForeignKey(TaxonClass, blank=True, null=True, on_delete=models.CASCADE, verbose_name="Kingdom")
-    taxon_order = models.ForeignKey(TaxonOrder, blank=True, null=True, on_delete=models.CASCADE, verbose_name="Kingdom")
-    taxon_family = models.ForeignKey(TaxonFamily, blank=True, null=True, on_delete=models.CASCADE, verbose_name="Kingdom")
-    taxon_genus = models.ForeignKey(TaxonGenus, blank=True, null=True, on_delete=models.CASCADE, verbose_name="Kingdom")
-    name = models.CharField(max_length=100)
-    class Meta:
-        verbose_name_plural = 'species'
-
-    def __str__(self):
-        return self.name
-
-class TaxonGenus(models.Model):
-    taxon_kingdom = models.ForeignKey(TaxonKingdom, blank=True, null=True, on_delete=models.CASCADE, verbose_name="Kingdom")
-    taxon_phylum = models.ForeignKey(TaxonPhylum, blank=True, null=True, on_delete=models.CASCADE, verbose_name="Kingdom")
-    taxon_class = models.ForeignKey(TaxonClass, blank=True, null=True, on_delete=models.CASCADE, verbose_name="Kingdom")
-    taxon_order = models.ForeignKey(TaxonOrder, blank=True, null=True, on_delete=models.CASCADE, verbose_name="Kingdom")
-    taxon_family = models.ForeignKey(TaxonFamily, blank=True, null=True, on_delete=models.CASCADE, verbose_name="Kingdom")
-    name = models.CharField(max_length=100)
-    class Meta:
-        verbose_name_plural = 'genera'
-
-    def __str__(self):
-        return self.name
-
-class TaxonFamily(models.Model):
-    taxon_kingdom = models.ForeignKey(TaxonKingdom, blank=True, null=True, on_delete=models.CASCADE, verbose_name="Kingdom")
-    taxon_phylum = models.ForeignKey(TaxonPhylum, blank=True, null=True, on_delete=models.CASCADE, verbose_name="Kingdom")
-    taxon_class = models.ForeignKey(TaxonClass, blank=True, null=True, on_delete=models.CASCADE, verbose_name="Kingdom")
-    taxon_order = models.ForeignKey(TaxonOrder, blank=True, null=True, on_delete=models.CASCADE, verbose_name="Kingdom")
-    name = models.CharField(max_length=100)
-    class Meta:
-        verbose_name_plural = 'families'
-
-    def __str__(self):
-        return self.name
-
-class TaxonOrder(models.Model):
-    taxon_kingdom = models.ForeignKey(TaxonKingdom, blank=True, null=True, on_delete=models.CASCADE, verbose_name="Kingdom")
-    taxon_phylum = models.ForeignKey(TaxonPhylum, blank=True, null=True, on_delete=models.CASCADE, verbose_name="Kingdom")
-    taxon_class = models.ForeignKey(TaxonClass, blank=True, null=True, on_delete=models.CASCADE, verbose_name="Kingdom")
-    name = models.CharField(max_length=100)
-    class Meta:
-        verbose_name_plural = 'orders'
-
-    def __str__(self):
-        return self.name
-
-class TaxonClass(models.Model):
-    taxon_kingdom = models.ForeignKey(TaxonKingdom, blank=True, null=True, on_delete=models.CASCADE, verbose_name="Kingdom")
-    taxon_phylum = models.ForeignKey(TaxonPhylum, blank=True, null=True, on_delete=models.CASCADE, verbose_name="Kingdom")
-    name = models.CharField(max_length=100)
-    class Meta:
-        verbose_name_plural = 'classes'
-
-    def __str__(self):
-        return self.name
-
-class TaxonPhylum(models.Model):
-    taxon_kingdom = models.ForeignKey(TaxonKingdom, blank=True, null=True, on_delete=models.CASCADE, verbose_name="Kingdom")
-    name = models.CharField(max_length=100)
-    class Meta:
-        verbose_name_plural = 'phyla'
-
-    def __str__(self):
-        return self.name
-
-class TaxonKingdom(models.Model):
-    name = models.CharField(max_length=100)
-    class Meta:
-        verbose_name_plural = 'kingdoms'
 
     def __str__(self):
         return self.name
