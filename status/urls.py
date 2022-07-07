1fcb5bbccdfc1ed94baa5d2a0f74ae56027edace
Tyler Alioto <tyler.alioto@gmail.com>
2022-07-07 12:21:47 +0200

    fixed urls.py

--- status/urls.py
+++ status/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 from status.views import TargetSpeciesListView
+from status.views import HomeView
 
 urlpatterns = [
     path("", HomeView.as_view(), name="home"),
