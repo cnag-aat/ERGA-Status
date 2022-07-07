e8485081e61a85889ba8f77500f6f4cf14790a3f
Tyler Alioto <tyler.alioto@gmail.com>
2022-07-07 23:42:17 +0200

    changed display string of target species

--- status/models.py
+++ status/models.py
@@ -103,7 +103,7 @@ class TargetSpecies(models.Model):
         verbose_name_plural = 'species'
 
     def __str__(self):
-        return self.tolid_prefix
+        return self.scientific_name +" ("self.tolid_prefix+")"
 
 class CommonNames(models.Model):
     species = models.ForeignKey(TargetSpecies, on_delete=models.CASCADE, verbose_name="Genus/species")
