fd12b1b2d91b2232e2b6a44f2bcab8a747a4122b
Tyler Alioto <tyler.alioto@gmail.com>
2022-07-08 14:12:15 +0200

    added order by taxonomy in targetspecies model

--- status/models.py
+++ status/models.py
@@ -101,6 +101,7 @@ class TargetSpecies(models.Model):
 
     class Meta:
         verbose_name_plural = 'species'
+        ordering = ['taxon_kingdom', 'taxon_phylum', 'taxon_class', 'taxon_order','taxon_family','taxon_genus','scientific_name']
 
     def __str__(self):
         return self.scientific_name +" (" + self.tolid_prefix + ")"
