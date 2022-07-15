12c604aa16732de3b9a4689eb49758ef90bf434d
Tyler Alioto <tyler.alioto@gmail.com>
2022-07-15 10:44:23 +0200

    fixed typo

--- status/urls.py
+++ status/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
     path("", HomeView.as_view(), name="home"),
     path("species/", TargetSpeciesListView.as_view(), name="species_list"),
     path("assemblies/", AssemblyListView.as_view(), name="assembly_list"),
-    path("projects/", ASsemblyProjectListView.as_view(), name="assembly_project_list"),
+    path("projects/", AssemblyProjectListView.as_view(), name="assembly_project_list"),
 ]
