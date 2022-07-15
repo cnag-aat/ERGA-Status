e63dfec4887a0c4f298cd96ef147586d05d640c1
Tyler Alioto <tyler.alioto@gmail.com>
2022-07-15 15:38:15 +0200

    add affiliation to team stringification

--- status/models.py
+++ status/models.py
@@ -167,7 +167,7 @@ class AssemblyTeam(models.Model):
         verbose_name_plural = 'assembly teams'
 
     def __str__(self):
-        return self.name
+        return self.name + " ("+self.affiation+")"
 
 
 class SampleCollection(models.Model):
