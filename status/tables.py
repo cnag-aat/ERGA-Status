865afad05e80c4cdde9a43478d5e30188acdbdd4
Tyler Alioto <tyler.alioto@gmail.com>
2022-07-19 23:59:37 +0200

    linked columns in species table

--- status/tables.py
+++ status/tables.py
@@ -7,14 +7,13 @@ from django.utils.safestring import mark_safe
 #import html
 class TargetSpeciesTable(tables.Table):
     export_formats = ['csv', 'tsv']
+    scientific_name = tables.Column(linkify=True)
+    tolid_prefix = tables.Column(linkify=True)
     class Meta:
         model = TargetSpecies
         template_name = "django_tables2/bootstrap4.html"
         paginate = {"per_page": 100}
-        #attrs = {"class": "table-sm"}
-        #fields = ('scaffold','start','end','orientation','roary_gene','gene','st')
-        #fields = ("name", )
-        #id = tables.Column(linkify=True)
+        fields = ('tolid_prefix', 'scientific_name','taxon_id', 'genome_size', 'c_value','ploidy','chromosome_number','haploid_number','taxon_kingdom','taxon_phylum','taxon_class','taxon','taxon_family','taxon_genus')
 
 class AssemblyTable(tables.Table):
     export_formats = ['csv', 'tsv']
