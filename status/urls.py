82dea0b8352fe7649d2f10b7aa3e62b726348d08
Tyler Alioto <tyler.alioto@gmail.com>
2022-07-23 01:18:57 +0200

    fixed typos

--- status/urls.py
+++ status/urls.py
@@ -14,11 +14,11 @@ urlpatterns = [
     path("assemblies/", AssemblyListView.as_view(), name="assembly_list"),
     path("projects/", AssemblyProjectListView.as_view(), name="assembly_project_list"),
     path("projects/?species=<scientific_name>", AssemblyProjectListView.as_view(), name="assembly_project_list"),path("assembly_team/<int:pk>/", views.assembly_team_detail, name="assembly_team_detail"),
-    path("colection_team/<int:pk>/", views.colection_team_detail, name="colection_team_detail"),
+    path("collection_team/<int:pk>/", views.collection_team_detail, name="collection_team_detail"),
     path("sequencing_team/<int:pk>/", views.sequencing_team_detail, name="sequencing_team_detail"),
     path("assembly_team/<int:pk>/", views.assembly_team_detail, name="assembly_team_detail"),
     path("curation_team/<int:pk>/", views.curation_team_detail, name="curation_team_detail"),
     path("annotation_team/<int:pk>/", views.annotation_team_detail, name="annotation_team_detail"),
     path("submission_team/<int:pk>/", views.submission_team_detail, name="submission_team_detail"),
-    
+
 ]
