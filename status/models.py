ae1abfd6bc970464a1cbeb01ebaf00499b03dc70
Tyler Alioto <tyler.alioto@gmail.com>
2022-07-25 04:47:14 +0200

    debug

--- status/models.py
+++ status/models.py
@@ -336,7 +336,7 @@ class Submission(models.Model):
         return self.species.tolid_prefix
 
 class AssemblyProject(models.Model):
-    species = models.ForeignKey(TargetSpecies, on_delete=models.CASCADE, verbose_name="species",related_name="project")
+    species = models.OneToOneField(TargetSpecies, on_delete=models.CASCADE, verbose_name="species")
     team = models.ForeignKey(AssemblyTeam, on_delete=models.CASCADE, verbose_name="assembly team")
     status = models.CharField(max_length=12, help_text='Status', choices=STATUS_CHOICES, default='Waiting')
     note = models.CharField(max_length=300, help_text='Notes', null=True, blank=True)
