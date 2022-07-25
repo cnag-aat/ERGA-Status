104c5dec045af3fd9391e5f33afbd03e81056c6a
Tyler Alioto <tyler.alioto@gmail.com>
2022-07-25 02:04:36 +0200

    debuggiing

--- status/tables.py
+++ status/tables.py
@@ -7,7 +7,7 @@ from django.utils.safestring import mark_safe
 #import html
 class OverviewTable(tables.Table):
     export_formats = ['csv', 'tsv']
-    assembly_status = tables.Column(accessor='assembly_project_list__status',
+    assembly_status = tables.Column(accessor='assemblyproject',
                          linkify=True, verbose_name='Assembly')
     tolid_prefix = tables.Column(linkify=True)
     class Meta:
