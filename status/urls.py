1fffc6a82601e2008a52142f532449ff931c53b6
Tyler Alioto <tyler.alioto@gmail.com>
2022-07-25 16:15:03 +0200

    debug

--- status/urls.py
+++ status/urls.py
@@ -29,7 +29,7 @@ urlpatterns = [
     path("curation_team/<int:pk>/", views.curation_team_detail, name="curation_team_detail"),
     path("annotation_team/<int:pk>/", views.annotation_team_detail, name="annotation_team_detail"),
     path("submission_team/<int:pk>/", views.submission_team_detail, name="submission_team_detail"),
-    url('collection/?species=<int:pk>', SampleCollectionListView.as_view(), name="collection_list"),
+    url('collection/?species=<int>', SampleCollectionListView.as_view(), name="collection_list"),
     path("sequencing/?species=<scientific_name>", SequencingListView.as_view(), name="sequencing_list"),
     path("curation/?species=<scientific_name>", CurationListView.as_view(), name="curation_list"),
     path("annotation/?species=<scientific_name>", AnnotationListView.as_view(), name="annotation_list"),
