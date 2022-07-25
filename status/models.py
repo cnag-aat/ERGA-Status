aa759c7cf2eb716b2f8beb41cbcb83ce665b31ac
Tyler Alioto <tyler.alioto@gmail.com>
2022-07-25 16:31:42 +0200

    changed reverse

--- status/models.py
+++ status/models.py
@@ -279,7 +279,7 @@ class SampleCollection(models.Model):
 
     def get_absolute_url(self):
         from django.urls import reverse
-        return reverse('collection_list', args=[str(self.species)])
+        return reverse('collection_list', args=[self.species])
 
     def __str__(self):
         return self.species.tolid_prefix
