07045e1b221ed7323dbe584993775509ad3dea81
Tyler Alioto <tyler.alioto@gmail.com>
2022-07-26 15:46:13 +0200

    debug

--- status/tables.py
+++ status/tables.py
@@ -5,6 +5,7 @@ from .models import *
 import re
 from django.utils.safestring import mark_safe
 from django.utils.html import escape
+from django.utils.html import format_html
 from django.urls import reverse
 #import html
 class OverviewTable(tables.Table):
