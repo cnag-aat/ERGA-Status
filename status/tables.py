2a77ae3b4a951c72abf03e1bcd79632dc78479be
Tyler Alioto <tyler.alioto@gmail.com>
2022-07-26 16:46:54 +0200

    debug

--- status/tables.py
+++ status/tables.py
@@ -128,7 +128,7 @@ class ReadsTable(tables.Table):
     def render_ont_yield(self, value, record):
         rs = Sequencing.objects.get(pk=record.project.pk)
         percent = value/rs.ont_target
-        return percent
+        return str(percent)
 
     class Meta:
         model = Reads
