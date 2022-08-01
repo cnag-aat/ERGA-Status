5bea3d57273d235536962cc8bd625cfb52d1422a
Tyler Alioto <tyler.alioto@gmail.com>
2022-08-01 16:35:39 +0200

    debug

--- status/tables.py
+++ status/tables.py
@@ -12,7 +12,7 @@ from status.models import *
 class OverviewTable(tables.Table):
     export_formats = ['csv', 'tsv','xls']
     # collection_status = tables.LinkColumn("collection_list",  kwargs={"species": tables.A("pk")},accessor='samplecollection.status',verbose_name='Collection')
-    genomic_sample_status = tables.Column(accessor='samplecollection.genomic_sample_status',verbose_name='Genomic Sample')
+    genomic_sample_status = tables.Column(accessor='samplecollection.genomic_sample_status',verbose_name='Genomic Sample',attrs={"td": {"bgcolor": "#547980"}})
     hic_sample_status = tables.Column(accessor='samplecollection.hic_sample_status',verbose_name='HiC Sample')
     rna_sample_status = tables.Column(accessor='samplecollection.rna_sample_status',verbose_name='RNA Sample')
     genomic_seq_status = tables.Column(accessor='sequencing.genomic_seq_status',verbose_name='gDNA-Seq')
