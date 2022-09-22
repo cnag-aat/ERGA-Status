3160e5bc40ce861bb1ef72d9d3a00047e09ae287
Tyler Alioto <tyler.alioto@gmail.com>
2022-09-22 16:00:28 +0200

    bugfix

--- status/tables.py
+++ status/tables.py
@@ -408,8 +408,10 @@ class SpecimenTable(tables.Table):
         paginate = {"per_page": 100}
 
 class SampleTable(tables.Table):
-    biosampleAccession = tables.TemplateColumn( '<a href="https://www.ebi.ac.uk/biosamples/samples/{{record.biosampleAccession}}"{{record.biosampleAccession}}</a>',empty_values=(), verbose_name='BioSample')
-    
+    # biosampleAccession = tables.TemplateColumn( '<a href="https://www.ebi.ac.uk/biosamples/samples/{{record.biosampleAccession}}"{{record.biosampleAccession}}</a>',empty_values=(), verbose_name='BioSample')
+    def render_biosampleAccession(self, value, record):
+        html = '<a href="https://www.ebi.ac.uk/biosamples/samples/'+value+'">'+escape(value)+'</a>'
+        return mark_safe(html)
     class Meta:
         model = Sample
         template_name = "django_tables2/bootstrap4.html"
