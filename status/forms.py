07824a7e724245d7125519db4eca302a2dacf376
Tyler Alioto <tyler.alioto@gmail.com>
2022-11-13 19:18:41 +0100

    debug

--- status/forms.py
+++ status/forms.py
@@ -1,26 +1,24 @@
 from allauth.account.forms import SignupForm
 from django import forms
 
-
+ROLE_CHOICES = (
+('assembly_team_lead', 'Assembly Team Lead'),
+('sequencing_team_lead', 'Sequencing Team Lead'),
+('annotation_team_lead', 'Annotation Team Lead'),
+('sample_coordinator', 'Sample Coordinator'),
+('genome_team_coordinator', 'Genome Team Coordinator'),
+('sample_reception', 'Sample Reception'),
+('assembly_curation', 'Assembly Curation'),
+('assembly_team_lead', 'Assembly Team Lead')
+)
 class CustomSignupForm(SignupForm):
-	ROLE_CHOICES = (
-        ('assembly_team_lead', 'Assembly Team Lead'),
-        ('sequencing_team_lead', 'Sequencing Team Lead'),
-        ('annotation_team_lead', 'Annotation Team Lead'),
-        ('sample_coordinator', 'Sample Coordinator'),
-        ('genome_team_coordinator', 'Genome Team Coordinator'),
-        ('sample_reception', 'Sample Reception'),
-        ('assembly_curation', 'Assembly Curation'),
-        ('assembly_team_lead', 'Assembly Team Lead')
-    )
 
     first_name = forms.CharField(max_length=30, label='First Name')
 	last_name = forms.CharField(max_length=30, label='Last Name')
-    roles = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple,
-                      choices=ROLE_CHOICES)
+    roles = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple,choices=ROLE_CHOICES)
     class Meta():
-    model = User
-    fields = ('roles', 'email','first_name','last_name','username')
+        model = User
+        fields = ('roles', 'email','first_name','last_name','username')
 
 	def save(self, request):
 		user = super(CustomSignupForm, self).save(request)
