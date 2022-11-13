a91f16e8ea0d83e2b4c959c156d5d43e063f083a
Tyler Alioto <tyler.alioto@gmail.com>
2022-11-13 19:23:10 +0100

    debug

--- status/forms.py
+++ status/forms.py
@@ -15,14 +15,14 @@ class CustomSignupForm(SignupForm):
     first_name = forms.CharField(max_length=30, label='First Name')
     last_name = forms.CharField(max_length=30, label='Last Name')
     roles = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple,choices=ROLE_CHOICES)
-    class Meta():
-        model = User
-        fields = ('roles', 'email','first_name','last_name','username')
+    # class Meta():
+    #     model = User
+    #     fields = ('roles', 'email','first_name','last_name','username')
 
-        def save(self, request):
-            user = super(CustomSignupForm, self).save(request)
-            roles = user_form.cleaned_data['roles']
-            user.first_name = self.cleaned_data['first_name']
-            user.last_name = self.cleaned_data['last_name']
-            user.save()
-            return user
+    def save(self, request):
+        user = super(CustomSignupForm, self).save(request)
+        roles = user_form.cleaned_data['roles']
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['last_name']
+        user.save()
+        return user
