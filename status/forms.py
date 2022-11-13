6e64231f0d8ea97570144297ed7a685fb0c7bd99
Tyler Alioto <tyler.alioto@gmail.com>
2022-11-13 19:12:38 +0100

    adding multiple choice

--- status/forms.py
+++ status/forms.py
@@ -3,13 +3,28 @@ from django import forms
 
 
 class CustomSignupForm(SignupForm):
-	first_name = forms.CharField(max_length=30, label='First Name')
+	ROLE_CHOICES = (
+    ('assembly_team_lead', 'Assembly Team Lead'),
+    ('sequencing_team_lead', 'Sequencing Team Lead'),
+    ('annotation_team_lead', 'Annotation Team Lead'),
+    ('sample_coordinator', 'Sample Coordinator'),
+    ('genome_team_coordinator', 'Genome Team Coordinator'),
+    ('sample_reception', 'Sample Reception'),
+    ('assembly_curation', 'Assembly Curation'),
+    ('assembly_team_lead', 'Assembly Team Lead'),)
+
+    first_name = forms.CharField(max_length=30, label='First Name')
 	last_name = forms.CharField(max_length=30, label='Last Name')
-	assembly_lead = forms.BoolianField(verbose_name=_('Assembly Team Lead'), default=False)
+    roles = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple,
+                      choices=ROLE_CHOICES)
+    class Meta():
+    model = User
+    fields = ('roles', 'email','first_name','last_name','username')
 
 	def save(self, request):
 		user = super(CustomSignupForm, self).save(request)
-		user.first_name = self.cleaned_data['first_name']
+		roles = user_form.cleaned_data['roles']
+        user.first_name = self.cleaned_data['first_name']
 		user.last_name = self.cleaned_data['last_name']
 		user.save()
 		return user
