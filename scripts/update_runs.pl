1f346857e37bece4871ac89a8409633bf9c8a319
talioto <tyler.alioto@cnag.crg.eu>
2024-02-07 15:31:07 +0100

    added new features to and fixed bugs in update_runs.pl

--- scripts/update_runs.pl
+++ scripts/update_runs.pl
@@ -1,11 +1,12 @@
 #!/usr/bin/env perl
 use lib "/home/groups/assembly/talioto/myperlmods/"; #change this to point to your PERL5LIB module directory or set the $PERL5LIB environment variable
-#use lib '/home/groups/assembly/talioto/erga_scripts';
+use lib '/home/groups/assembly/talioto/erga_scripts';
 use REST::Client;
 use MIME::Base64;
 use JSON::PP;
 use Getopt::Long;
 use strict;
+use File::Basename qw( fileparse );
 
 # https://metacpan.org/pod/REST::Client
 # https://metacpan.org/pod/JSON::PP
@@ -19,7 +20,17 @@ my $erga_status_url="https://genomes.cnag.cat/erga-stream/api";
 my $printhelp = 0;
 my $seq_data;
 my $sequencing_tsv_file = 0;
-
+my %SEQUENCING_STATUS_CHOICES = (
+  'Waiting'=>1,
+  'Received'=>1,
+  'Extracted'=>1,
+  'Sequencing'=>1,
+  'TopUp'=>1,
+  'External'=>1,
+  'Submitted'=>1,
+  'Done'=>1,
+  'Issue'=>1
+);
 
 GetOptions(
   'c|config:s' => \$conf,
@@ -84,6 +95,9 @@ END_HELP
 if ($printhelp or !$sequencing_tsv_file){
   print $usage and exit;
 }
+my ($base,$path,$ext) = fileparse($sequencing_tsv_file,qw(\.tsv \.tab \.txt));
+my $outfile = "$base.withBiosample.tsv";
+open (OUT, ">$outfile");
 open(CONF,"<$conf") or die "Configuration file $conf does not exist. Please create it in the current working directory or specify the path to it using the -c option.";
 my %config = ();
 while(my $l = <CONF>){
@@ -134,11 +148,12 @@ sub update{
         native_file_name 
         native_file_md5
         );
-  print join("\t",@fields),"\n"; #print header of returned output table, with retrieved biosample accession added.
+  print OUT join("\t",@fields),"\n"; #print header of returned output table, with retrieved biosample accession added.
   for (my $i = 0;$i<@$sequpdate; $i++){
     my $tolid_prefix=$sequpdate->[$i]->{'tolid'};
     $tolid_prefix =~ s/\d+$//;
     my $scientific_name=$sequpdate->[$i]->{'scientific_name'};
+    print STDERR "$scientific_name\n";
     my $instrument=$sequpdate->[$i]->{'instrument'};
     my $library_strategy=$sequpdate->[$i]->{'library_strategy'};
     my $out_library_strategy = "WGS";
@@ -151,11 +166,13 @@ sub update{
     my $yield=$sequpdate->[$i]->{'yield'};
     next if not $yield > 0;
     next if $yield eq '';
+    print STDERR "yield: $yield\n";
     my $species_id = 0;
     my $species_url = 0;
     if ($tolid_prefix =~m/\w/){
       #Retrieve species from target species table
       $client->GET("$erga_status_url/species/?tolid_prefix=". $tolid_prefix);
+          print STDERR $client->responseContent(),"\n\n";
       my $response1 = decode_json $client->responseContent();
       $species_url = $response1->{results}->[0]->{url};
       $species_url =~/(\d+)\/$/;
@@ -163,6 +180,7 @@ sub update{
     }elsif($scientific_name =~m/\w/){
       #Retrieve species from target species table
       $client->GET("$erga_status_url/species/?scientific_name=". $scientific_name);
+          print STDERR $client->responseContent(),"\n\n";
       my $response1 = decode_json $client->responseContent();
       $species_url = $response1->{results}->[0]->{url};
       $species_url =~/(\d+)\/$/;
@@ -173,45 +191,89 @@ sub update{
 
     #Retrieve project based on species_id
     $client->GET("$erga_status_url/sequencing/?species=". $species_id);
+          print STDERR $client->responseContent(),"\n\n";
     my $response2 = decode_json $client->responseContent();
     my $project_url = $response2->{results}->[0]->{url};
     $project_url =~/(\d+)\/$/;
     my $project_id = $1;
+    print STDERR "project id: $project_id\n";
 
     if ($response2->{count} == 1) { #proceed if there is one and only one project
       $sequpdate->[$i]->{project}=$project_id;
       my $tubestring = $sequpdate->[$i]->{'sample_tube_or_well_id'};
       my $first_tube = $tubestring;
       my $pool = 0;
+      my @tubes = ();
+      my $sample_url = '';
       if ($tubestring=~/[,;]/){
-        my @tubes = split /[,;]/, $tubestring;
+        $tubestring =~ s/\s//g;
+        @tubes = split /[,;]/, $tubestring;
         $first_tube = $tubes[0];
         $pool = 1;
-      }
+      }else{push @tubes, $tubestring;}
       if ($sequpdate->[$i]->{'yield'} =~/\S/){
-        my $sample_url = '';
-        my $squery = "$erga_status_url/sample/?tube_or_well_id=".$first_tube ;
-        $client->GET($squery);
-        my $response_sample = decode_json $client->responseContent();
-        #print STDERR $client->responseContent(),"\n\n";
-        if ($response_sample->{count} > 0) {
-          $sequpdate->[$i]->{'biosample_accession'} = $response_sample->{results}->[0]->{biosampleAccession};
-          if ($pool){
-            $sequpdate->[$i]->{'biosample_accession'} = $response_sample->{results}->[0]->{sampleDerivedFrom};
+        my @biosample_accessions = ();
+        foreach my $t (@tubes){
+          ### try tube id first
+          my $squery = "$erga_status_url/sample/?tube_or_well_id=".$t ;
+          $client->GET($squery);
+          print STDERR $client->responseContent(),"\n\n";
+          my $response_sample = decode_json $client->responseContent();
+
+          if ($response_sample->{count} > 0) {
+            #$sequpdate->[$i]->{'sampleDerivedFrom'} = $response_sample->{results}->[0]->{sampleDerivedFrom};
+            push @biosample_accessions, $response_sample->{results}->[0]->{biosampleAccession};
+            # if ($pool){
+            #   $sequpdate->[$i]->{'biosample_accession'} = $response_sample->{results}->[0]->{sampleDerivedFrom};
+            # }
+            $sample_url = $response_sample->{results}->[0]->{url};
+            $client->GET($response_sample->{results}->[0]->{specimen});
+            print STDERR $client->responseContent(),"\n\n";
+            my $response_specimen = decode_json $client->responseContent();
+            #print STDERR "tolid: ",$response_specimen->{tolid},"\n\n";
+            $sequpdate->[$i]->{'tolid'} = $response_specimen->{tolid};
+          }else{
+            $squery = "$erga_status_url/sample/?gal_sample_id=".$t ;
+            $client->GET($squery);
+            my $response_sample_gal_sample_id = decode_json $client->responseContent();
+            print STDERR $client->responseContent(),"\n\n";
+            if ($response_sample_gal_sample_id->{count} > 0) {
+              #$sequpdate->[$i]->{'sampleDerivedFrom'} = $response_sample->{results}->[0]->{sampleDerivedFrom};
+              push @biosample_accessions, $response_sample_gal_sample_id->{results}->[0]->{biosampleAccession};
+              # if ($pool){
+              #   $sequpdate->[$i]->{'biosample_accession'} = $response_sample->{results}->[0]->{sampleDerivedFrom};
+              # }
+              $sample_url = $response_sample_gal_sample_id->{results}->[0]->{url};
+              $client->GET($response_sample_gal_sample_id->{results}->[0]->{specimen});
+              #print STDERR $client->responseContent(),"\n\n";
+              my $response_specimen = decode_json $client->responseContent();
+              #print STDERR "tolid: ",$response_specimen->{tolid},"\n\n";
+              $sequpdate->[$i]->{'tolid'} = $response_specimen->{tolid};
+            }else{
+              print STDERR "No sample found for $scientific_name with tube_or_well_id or gal_sample_id:",$t,"\n";
+            }
           }
-          $sample_url = $response_sample->{results}->[0]->{url};
-          $client->GET($response_sample->{results}->[0]->{specimen});
-          #print STDERR $client->responseContent(),"\n\n";
-          my $response_specimen = decode_json $client->responseContent();
-          #print STDERR "tolid: ",$response_specimen->{tolid},"\n\n";
-          $sequpdate->[$i]->{'tolid'} = $response_specimen->{tolid};
-        }else{
-          print STDERR "No sample found for $scientific_name with tube_or_well_id:",$first_tube,"\n";
+
         }
+        $sequpdate->[$i]->{'biosample_accession'} = join(",",@biosample_accessions);
+        
 
         $client->GET("$erga_status_url/reads/?project=". $project_id);
+        print STDERR "$erga_status_url/reads/?project=$project_id\n";
+        print STDERR $client->responseContent();
+        my $reads_url = '';
         my $readsresp = decode_json $client->responseContent();
-        my $reads_url = $readsresp->{results}->[0]->{url};
+        if ($readsresp->{count} == 0) {
+          #reads record doesn't exist. Need to insert it.
+          my %read_rec = ();
+          $read_rec{'project'}=$project_url;
+          $client->POST("$erga_status_url/reads/", encode_json \%read_rec);
+          $client->GET("$erga_status_url/reads/?project=". $project_id);
+          my $readsresp2 = decode_json $client->responseContent();
+          $reads_url = $readsresp2->{results}->[0]->{url};
+        }else{
+          $reads_url = $readsresp->{results}->[0]->{url};
+        }
         my %read_insert_data = ();
         $read_insert_data{project}=$project_url;
         $read_insert_data{tube_or_well_id}=$sequpdate->[$i]->{'sample_tube_or_well_id'};
@@ -224,10 +286,10 @@ sub update{
         $read_insert_data{native_filename}=$sequpdate->[$i]->{'native_file_name'};
         $read_insert_data{native_md5sum}=$sequpdate->[$i]->{'native_file_md5'};
         $read_insert_data{reads}=$reads_url;
-        $read_insert_data{sample}=$sample_url;
+        $read_insert_data{sample}=$sequpdate->[$i]->{'biosample_accession'}; #$sample_url;
         my $readinsert = encode_json \%read_insert_data;
         
-        $client->GET("$erga_status_url/run/?sample_tube_or_well_id=". $sequpdate->[$i]->{'sample_tube_or_well_id'} ."&read_type=".$read_type ."&forward_filename=".$sequpdate->[$i]->{'forward_file_name'});
+        $client->GET("$erga_status_url/run/?read_type=".$read_type ."&forward_filename=".$sequpdate->[$i]->{'forward_file_name'});
         my $response_reads = decode_json $client->responseContent();
         if ($response_reads->{count} > 0) {
           #PATCH
@@ -235,29 +297,32 @@ sub update{
           my $run_url = $response_reads->{results}->[0]->{url};
           $client->PATCH($run_url, $readinsert);
           print STDERR "\n";
-          #print STDERR $client->responseContent(),"\n";
+          print STDERR $client->responseContent(),"\n";
         }else{
           #POST
           print STDERR "Inserting new run data... \n";
           $client->POST("$erga_status_url/run/", $readinsert);
-          #print STDERR $client->responseContent(),"\n";
+          print STDERR "$readinsert\n";
+          print STDERR $client->responseContent(),"\n";
         }
         #print back table
         $sequpdate->[$i]->{'library_selection'} = 'RANDOM';
         foreach my $f (@fields){
           if (exists($sequpdate->[$i]->{$f}) && $sequpdate->[$i]->{$f} =~ /\S/){
-            print $sequpdate->[$i]->{$f};
+            print OUT $sequpdate->[$i]->{$f};
           }else{
-            print '-';
+            print OUT '-';
           }
-          print "\t";
+          #print (exists($sequpdate->[$i]->{$f}) && $sequpdate->[$i]->{$f} =~ /\S/)?$sequpdate->[$i]->{$f}:"-";
+          print OUT "\t";
         }
-        print "\n";
+        print OUT "\n";
       }
     } else {
       print STDERR "Couldn't find project. Please add project for $scientific_name $tolid_prefix via the admin interface. Skipping for now.\n"; 
     }
   }
+  close OUT;
   return 1;
 }
 
