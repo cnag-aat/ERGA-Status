23fc93f9cf82852940d03aaeae807be16ae5bf82
talioto <tyler.alioto@cnag.crg.eu>
2024-04-26 13:54:45 +0200

    improved add_assembly.pl

--- scripts/add_assembly.pl
+++ scripts/add_assembly.pl
@@ -1,4 +1,5 @@
 #!/usr/bin/env perl
+use lib '/home/groups/assembly/talioto/erga_scripts';
 use REST::Client;
 use MIME::Base64;
 use JSON::PP;
@@ -7,37 +8,43 @@ use Getopt::Long;
 my $conf = ".ergastream.cnf";
 my $erga_status_url="https://genomes.cnag.cat/erga-stream/api";
 my $printhelp = 0;
+my $assembly_data;
+my $assembly_tsv_file = 0;
+my $verbose = 0;
 GetOptions(
 	   'c|config:s' => \$conf,
+     'file|f:s' => \$assembly_tsv_file,
+     'v' => \$verbose,
      'h|help' => \$printhelp
 	  );
 my $useage = <<'END_HELP';
-usage: $0 -c <ergastream.cnf>
+usage: $0 -c <ergastream.cnf> -f assemblies.tsv
  
-The ergastream.cnf file has the format:
-URL:https://genomes.cnag.cat/erga-stream/api
-username:<username>
-password:<password>
+  The ergastream.cnf file has the format:
+  URL:https://genomes.cnag.cat/erga-stream/api
+  username:<username>
+  password:<password>
 
-If using the development server or the URL changes at any time you can replace the url
-The username and passwords are the ones assigned to your team. If you'd like to use one attached to an email, 
-let Tyler know and he will grant your registered user the same priveleges.
+  If using the development server or the URL changes at any time you can replace the url
+  The username and passwords are the ones assigned to your team. If you'd like to use one attached to an email, 
+  let Tyler know and he will grant your registered user the same priveleges.
 
-EXAMPLE assemblies.tsv file:
-tolid_prefix	description	pipeline  pipeline_version	type	span	chr_level	pct_placed	contig_n50	scaffold_n50	busco	busco_db	busco_version	qv	report
-mRupRup	dummy_mRupRup1.1_pri	CNAG_Assembly  2.0	Primary	3000000000	True	98	10000000	120000000	C:95.9%[S:94.6%,D:1.3%],F:1.4%,M:2.7%,n:3354	vertebrata_odb10	5.2.2	45.3	
+  EXAMPLE assemblies.tsv (BUSCO string should be quoted or commas replaced with semicolons):
+  tolid_prefix,description,pipeline version,type,span,contig_n50,scaffold_n50,chromosome_level,percent_placed,busco,busco_db,busco_version,qv
+  odPhaVent,ratatosk.nextdenovo.hypo1.purged,CLAWS 2.1,Primary,207000000,2400000,6900000,FALSE,0,C:80.5%[S:78.4%;D:2.1%];F:8.5%;M:11.0%;n:954,metazoa_odb10,4.0.6,32.3
+  ilHelHell,nextdenovo.assembly.hypo1.purged.curated,CLAWS 2.0,Primary,547306268,23624374,23624374,TRUE,100,C:98.0%[S:97.7%;D:0.3%];F:0.7%;M:1.3%;n:1013,arthropoda_odb10,4.0.6,46.6
 
-ASSEMBLY_TYPE_CHOICES for the "type" field
-    Primary
-    Alternate
-    Hap1
-    Hap2
-    Maternal
-    Paternal
-    MT
-    Chloroplast
-    Endosymbiont
-
+  ASSEMBLY_TYPE_CHOICES = (
+    ('Primary', 'Pseudohaploid Primary'),
+    ('Alternate', 'Pseudohaploid Alternate'),
+    ('Hap1', 'Phased Haplotype 1'),
+    ('Hap2', 'Phased Haplotype 2'),
+    ('Maternal', 'Trio-phased Maternal'),
+    ('Paternal', 'Trio-phased Paternal'),
+    ('MT', 'Mitogenome'),
+    ('Chloroplast', 'Chloroplast'),
+    ('Endosymbiont', 'Endosymbiont')
+  )
 END_HELP
 
 if ($printhelp){
@@ -62,16 +69,10 @@ $client->addHeader('Accept', 'application/json');
 $client->addHeader('Authorization' => 'Basic '.encode_base64($config{'username'}.":".$config{'password'}));
 
 #### EXAMPLE TABLES ####
+#assemblies.tsv
+#tolid_prefix,description,pipeline,type,span,contig_n50,scaffold_n50,chromosome_level,percent_placed,busco,busco_db,busco_version,qv
 
-#assemblies.csv
-#tolid_prefix,description,type,span,contig_n50,scaffold_n50,chromosome_level,percent_placed,busco,busco_db,busco_version,qv
-#odPhaVent,ratatosk.nextdenovo.hypo1.purged,Primary,207000000,2400000,6900000,FALSE,0,C:80.5%[S:78.4%;D:2.1%];F:8.5%;M:11.0%;n:954,metazoa_odb10,4.0.6,32.3
-#ilHelHell,nextdenovo.assembly.hypo1.purged.curated,Primary,547306268,23624374,23624374,TRUE,100,C:98.0%[S:97.7%;D:0.3%];F:0.7%;M:1.3%;n:1013,arthropoda_odb10,4.0.6,46.6
-
-my $assembly_data;
-my $assembly_tsv_file = 0;
-$assembly_csv_file = shift or print $usage and exit;
-die "No assembly data!\n" unless $assembly_data=(loadTbl($assembly_csv_file,$assembly_data));
+die "No assembly data!\n" unless $assembly_data=(loadTbl($assembly_tsv_file,$assembly_data));
 print STDERR "Parsed data files... ready to add to ERGA-Status.\n";
 print STDERR "Adding assembly data...\n"; #print STDERR Data::Dumper->Dump($assembly_data),"\n" and exit;
 add_assembly($assembly_data);
@@ -98,17 +99,14 @@ sub add_assembly{
     #Retrieve project based on species_id
     $client->GET("$erga_status_url/assembly_project/?species=". $species_id);
     my $response2 = decode_json $client->responseContent();
-    #print STDERR $client->responseContent() and exit;
     my $project_url = $response2->{results}->[0]->{url};
     $project_url =~/(\d+)\/$/;
     #print STDERR "$project_url\n";
     my $project_id = $1;
-
-
-
     if ($response2->{count} == 1) { #proceed if there is one and only one project
       $assemblies->[$i]->{project}=$project_id;
       my $d=$assemblies->[$i]->{description};
+      my $assemblypipeline=$assemblies->[$i]->{pipeline};
       my $type=$assemblies->[$i]->{type};
       my $span=$assemblies->[$i]->{span};
       my $chr_level=$assemblies->[$i]->{chr_level};
@@ -119,34 +117,51 @@ sub add_assembly{
       my $busco_version=$assemblies->[$i]->{busco_version};
       my $report=$assemblies->[$i]->{report};
 
-      #Get the busco_db. If it doesn't exist, add it via the admin interface and redo this. In future, we can do it here.
-      $client->GET("$erga_status_url/busco_db/?db=". $busco_db);
-      my $busco_db_response = decode_json $client->responseContent();
-      my $busco_db_url = $busco_db_response->{results}->[0]->{url};
-
-      #Get the BUSCO version. If it doesn't exist, add it via the admin interface and redo this. In future, we can do it here.
-      $client->GET("$erga_status_url/busco_version/?version=". $busco_version);
-      my $buso_version_response = decode_json $client->responseContent();
-      my $busco_version_url = $busco_version_response->{results}->[0]->{url};
-
-      #check to see if the assembly already exists. Give warning.
-      my $query = "$erga_status_url/assembly/?project=$project_id&description=$d&contig_n50=$cn50&scaffold_n50=$sn50&qv=$qv";
-      #print "$query\n";
-      $client->GET($query);
-      my $response3 = decode_json $client->responseContent();
-      if ($response3->{count} > 0) {
-        print STDERR "Possible duplicate: $d\n",encode_json($response3),"\nAdding anyway. You may want to manually remove this record.\n";
+      #Get the pipeline. If it doesn't exist, add it via the admin interface and come back and do this again.
+      if (length($assemblypipeline)){
+        die "pipeline must be in format <name> <version>" if $assemblypipeline !~/\S\s\S/;
+        my ($pipeline_name,$pipeline_version)= split /\s/,$assemblypipeline;
+        my $pipeline_response = make_request($client,"$erga_status_url/assembly_pipeline/?name=$pipeline_name&version=$pipeline_version");
+        my $pipeline_url = '';
+        if ($pipeline_response->{count} > 0) {
+          $pipeline_url = $pipeline_response->{results}->[0]->{url};
+        }else{
+          print STDERR "Pipeline $assemblypipline not found: please add it using the admin panel\n";
+        }
       }
+      
+
+      #Get the busco_db. If it doesn't exist, add it.
+      my $busco_db_response = make_request($client,"$erga_status_url/busco_db/?db=". $busco_db);
+      my $busco_db_url = '';
+      if ($busco_db_response->{count} > 0) {
+        $busco_db_url = $busco_db_response->{results}->[0]->{url};
+      }else{
+        my $busco_entry;
+        $busco_entry->{db}=$busco_db;
+        my $post_resp = make_post($client,"$erga_status_url/busco_db/", encode_json($busco_entry));
+      }
+      #Get the BUSCO version. If it doesn't exist, add it.
+      my $busco_version_response = make_request($client,"$erga_status_url/busco_version/?version=$busco_version");
+      my $busco_version_url = $busco_version_response->{results}->[0]->{url};
       #Replace regular strings with REST URLs.
       $assemblies->[$i]->{project}=$project_url;
       $assemblies->[$i]->{busco_db}=$busco_db_url;
       $assemblies->[$i]->{busco_version}=$busco_version_url;
+      $assemblies->[$i]->{pipeline}=pipeline_url;
 
       #wrap it all up in JSON string
       my $insert = encode_json $assemblies->[$i];
-      #print STDERR "$insert\n";
-      $client->POST("$erga_status_url/assembly/", $insert);
-      print STDERR "Inserting... Response:",$client->responseContent(),"\n";
+      #check to see if the assembly already exists. patch entry if it does.
+      my $query = "$erga_status_url/assembly/?project=$project_id&description=$d&contig_n50=$cn50&scaffold_n50=$sn50&qv=$qv";
+      my $response3 = make_request($client,$query);
+      if ($response3->{count} > 0) {
+        print STDERR "Possible duplicate: $d\n",encode_json($response3),"\nPatching...\n";
+          my $found_assembly_url = $response3->{results}->[0]->{url};
+          my $patch_response = make_patch($client,$found_assembly_url, $insert);
+      }else{
+        my $post_resp = make_post($client,"$erga_status_url/assembly/", $insert);
+      }
     } else {
       print STDERR "Couldn't find project. Please add project for $tolid_prefix via the admin interface. Skipping for now.\n"; #print STDERR "Project could not be found in the samples table. Please add sample first.\n",encode_json($response2),"\n";
     }
@@ -173,4 +188,72 @@ sub loadTbl {
   }
   close TAB;
   return $count?$arrayref:0;
+}
+sub make_request{
+  my $client= shift;
+  my $query = shift;
+  print STDERR "$query\n" if $verbose;
+  my $response = "";
+    for (my $i=0; $i<10; $i++){
+      $client->GET($query);
+      $response = $client->responseContent();
+      last if $response !~ /timeout/;
+      sleep 1;
+    }
+    if ($response =~ /timeout/){
+      return 0;
+    }elsif ($response =~ /html/){
+      print STDERR "$response\n";
+      return 0;
+    }else{
+      print STDERR "$response\n" if $verbose;
+      return decode_json $response;
+    }
+}
+sub make_patch{
+  my $client= shift;
+  my $url = shift;
+  my $insert = shift;
+  print STDERR "$url\n$insert\n" if $verbose;
+  my $response = "";
+    for (my $i=0; $i<10; $i++){
+      $client->PATCH($url,$insert);
+      $response = $client->responseContent();
+      last if $response !~ /timeout/;
+      sleep 1;
+    }
+    if ($response =~ /timeout/){
+      return 0;
+    }elsif ($response =~ /html/){
+      print STDERR "$response\n";
+      return 0;
+    }else{
+      print STDERR "$response\n" if $verbose;
+      return decode_json $response;
+    }
+}
+sub make_post{
+  my $client= shift;
+  my $url = shift;
+  my $insert = shift;
+  print STDERR "$url\n$insert\n" if $verbose;
+  my $response = "";
+    for (my $i=0; $i<10; $i++){
+      $client->POST($url,$insert);
+      $response = $client->responseContent();
+      last if $response !~ /timeout/;
+      sleep 1;
+    }
+    if ($response =~ /timeout/){
+      return 0;
+    }elsif ($response =~ /html/){
+      print STDERR "$response\n";
+      return 0;
+    }elsif ($response =~ /Error/){
+      print STDERR "$response\n";
+      return 0;
+    }else{
+      print STDERR "$response\n" if $verbose;
+      return decode_json $response;
+    }
 }
